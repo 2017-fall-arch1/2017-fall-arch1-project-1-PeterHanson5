93f388bf8870139e4eb3389b2ea3f90a47157983
student <student@localhost.localdomain>
2017-10-23 09:45:33 -0600

    Updated version pf file

--- main.c
+++ main.c
@@ -5,33 +5,41 @@
 
 int main() {
   TreeNode *root = NULL;
-  char *name = (char *)malloc(40);
-  char choice = 'x';
+  char *name = (char *)malloc(100);
+  //char choice = 'x';
+  // char *choice = (char *)malloc(100);
+  char choice[100] = "x";
 
-  while(choice != 'q') {
+  while(strcmp(choice,"q") != 0) {
     printf("Please make your choice of the following:\n");
     printf("I. Insert Name\n");
     printf("D. Delete Name\n");
     printf("P. Print Names of Empolyees\n");
     printf("q. Exit the program\n");
-    scanf("%s",&choice);
+    // choice = fgets(choice, 100, stdin);
+    scanf("%s",choice);
+    int ret;
+    ret = strcmp(choice,"I");
+    printf("Strcmp %d\n%s\n",ret,choice);
 
-    if(choice == 'I') {
+    if(strcmp(choice,"I") == 0) {
       printf("Please enter name of employee:\n");
-      name = fgets(name, 40, stdin);
+      //fgets(name, 100, stdin);
+      scanf("%s",name);
       root = Insert(root, name);
     }
 
-    else if(choice == 'D') {
+    else if(strcmp(choice,"D") == 0) {
       printf("Please enter name of employee you like to delete:\n");
-      name = fgets(name, 40, stdin);
+      //fgets(name, 100, stdin);
+      scanf("%s",name);
       root = Delete(root, name);
     }
 
-    else if(choice == 'P') {
+    else if(strcmp(choice,"P") == 0) {
       PrintNames(root);
     }
   }
 }
 
-  name = fgets(name, 40, stdin);
+// name = fgets(name, 40, stdin);
